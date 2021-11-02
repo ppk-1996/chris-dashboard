5bbaca5df0e694eb7660ca674db95177193fdda7
Pyae Phyo Kyaw <59681592+ppk-1996@users.noreply.github.com>
2021-11-03 04:55:53 +0630

    testing library now use typescript

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,21 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  roots: ['<rootDir>/src'],
+  roots: ["<rootDir>/src"],
   transform: {
-    '^.+\\.tsx$': 'ts-jest',
-    '^.+\\.ts$': 'ts-jest',
+    "^.+\\.tsx$": "ts-jest",
+    "^.+\\.ts$": "ts-jest",
   },
-  testRegex: '(/__tests__/.*.(test|spec)).(jsx?|tsx?)$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["./jest.setup.ts"],
+  testRegex: "(/__tests__/.*.(test|spec)).(jsx?|tsx?)$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   collectCoverage: false,
   // collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
   // coverageDirectory: '<rootDir>/coverage/',
   // coveragePathIgnorePatterns: ['(tests/.*.mock).(jsx?|tsx?)$', '(.*).d.ts$'],
   moduleNameMapper: {
-    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2|svg)$':
-      'identity-obj-proxy',
+    ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2|svg)$":
+      "identity-obj-proxy",
   },
   verbose: true,
   testTimeout: 30000,
