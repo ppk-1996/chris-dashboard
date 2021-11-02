6746bd0aa907c566eefe115fd2e6f18119c21400
Pyae Phyo Kyaw <59681592+ppk-1996@users.noreply.github.com>
2021-11-03 04:29:03 +0630

    testing coverage disable

--- jest.config.js
+++ jest.config.js
@@ -6,10 +6,10 @@ module.exports = {
   },
   testRegex: '(/__tests__/.*.(test|spec)).(jsx?|tsx?)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
-  coverageDirectory: '<rootDir>/coverage/',
-  coveragePathIgnorePatterns: ['(tests/.*.mock).(jsx?|tsx?)$', '(.*).d.ts$'],
+  collectCoverage: false,
+  // collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
+  // coverageDirectory: '<rootDir>/coverage/',
+  // coveragePathIgnorePatterns: ['(tests/.*.mock).(jsx?|tsx?)$', '(.*).d.ts$'],
   moduleNameMapper: {
     '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2|svg)$':
       'identity-obj-proxy',
